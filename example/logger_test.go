42f75d52b3f9205b6929aba71a10b40202a6c979
Harvey <hsyan2008@qq.com>
2018-12-07 13:31:35 +0800

    fix test

--- example/logger_test.go
+++ example/logger_test.go
@@ -1,11 +1,12 @@
 package example
 
 import (
-	"github.com/hsyan2008/go-logger/logger"
 	"runtime"
 	"strconv"
 	"testing"
 	"time"
+
+	logger "github.com/hsyan2008/go-logger"
 )
 
 func log(i int) {
@@ -22,17 +23,23 @@ func Test(t *testing.T) {
 	//指定是否控制台打印，默认为true
 	logger.SetConsole(true)
 	//指定日志文件备份方式为文件大小的方式
-	//第一个参数为日志文件存放目录
-	//第二个参数为日志文件命名
-	//第三个参数为备份文件最大数量
-	//第四个参数为备份文件大小
-	//第五个参数为文件大小的单位
-	//logger.SetRollingFile("d:/logtest", "test.log", 10, 5, logger.KB)
+	//第一个参数为日志文件
+	//第二个参数为备份文件最大数量
+	//第三个参数为备份文件大小
+	//第四个参数为文件大小的单位
+	// if runtime.GOOS == "windows" {
+	// 	logger.SetRollingFile("d:/test.log", 10, 5, "KB")
+	// } else {
+	// 	logger.SetRollingFile("/tmp/test.log", 10, 5, "KB")
+	// }
 
 	//指定日志文件备份方式为日期的方式
-	//第一个参数为日志文件存放目录
-	//第二个参数为日志文件命名
-	logger.SetRollingDaily("d:/logtest", "test.log")
+	//第一个参数为日志文件
+	if runtime.GOOS == "windows" {
+		logger.SetRollingDaily("d:/test.log")
+	} else {
+		logger.SetRollingDaily("/tmp/test.log")
+	}
 
 	//指定日志级别  ALL，DEBUG，INFO，WARN，ERROR，FATAL，OFF 级别由低到高
 	//一般习惯是测试阶段为debug，生成环境为info以上
